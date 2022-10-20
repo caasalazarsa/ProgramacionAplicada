43f3b46971e2e75208d52ae25d603ea5f75c4e7f
caasalazarsa <caasalazarsa@unal.edu.co>
2022-10-20 15:28:43 -0500

    all statees functional

--- DEMO_PROJECT/scripts/player.gd
+++ DEMO_PROJECT/scripts/player.gd
@@ -3,14 +3,15 @@ extends KinematicBody2D
 
 var state =1
 
-var gravity=100
+var gravity=400
 
 var walk_time_start=0
 var walk_time_now=0
 var walk_flag=false
 export var walk_velocity=100
 export var run_velocity=400
-export var jump_speed=300
+export var jump_velocity=0
+export var jump_speed=600
 
 var velocity=Vector2(0,0)
 # Declare member variables here. Examples:
@@ -36,33 +37,36 @@ func _process(delta):
 		1:
 			quieto()
 		2:
-			caminar()
+			caminata()
 		3:
-			correr()
+			sprint()
 		4:	
-			saltar()
+			salto()
 			
 	
 	pass
 	
 func _physics_process(delta):
-	move_and_slide(velocity,Vector2(1,0))
+	move_and_slide(velocity,Vector2(0,-1))
 
 func quieto():
 	print("estoy quieto")
 	if(Input.is_action_pressed("ui_left") or Input.is_action_pressed("ui_right")):
 		state=2	
 		
-	if(Input.is_action_just_pressed("ui_accept")):
+	if(Input.is_action_just_pressed("ui_accept") and is_on_floor()):
+		jump_velocity=walk_velocity
 		state=4
+		velocity.y=velocity.y-jump_speed
+		
 		
 		
 	pass
 	
-func caminar():
+func caminata():
 	walk_time_now=OS.get_unix_time()
 	
-	velocity.x=(int(Input.is_action_pressed("ui_right"))-int(Input.is_action_pressed("ui_left")))*walk_velocity
+	moverse(walk_velocity)
 	
 	if(Input.is_action_pressed("ui_right")):
 		$Sprite.flip_h=false
@@ -76,15 +80,16 @@ func caminar():
 	if(Input.is_action_pressed("ui_cancel")):
 		state=3
 		
-	if(Input.is_action_just_pressed("ui_accept")):
+	if(Input.is_action_just_pressed("ui_accept") and is_on_floor()):
+		jump_velocity=walk_velocity
 		state=4
+		velocity.y=velocity.y-jump_speed
 		
 	pass
 	
-func correr():
+func sprint():
 	
-	velocity.x=(int(Input.is_action_pressed("ui_right"))-int(Input.is_action_pressed("ui_left")))*run_velocity
-	
+	moverse(run_velocity)
 	if(Input.is_action_pressed("ui_right")):
 		$Sprite.flip_h=false
 		print("corro hacia la derecha")
@@ -96,19 +101,28 @@ func correr():
 	if Input.is_action_just_released("ui_cancel"):
 		state=2	
 		
-	if(Input.is_action_just_pressed("ui_accept")):
+	if(Input.is_action_just_pressed("ui_accept") and is_on_floor()):
+		jump_velocity=run_velocity
 		state=4
+		velocity.y=velocity.y-jump_speed
+		
 	
 	pass	
 	
-func saltar():
-	velocity.y=velocity.y-jump_speed
+func salto():
+	moverse(jump_velocity)
+	print("estoy en el aire")
+	if(is_on_floor()):
+		state=1
 	pass
 
 func gravity_effect(delta):
 	if velocity.y<400:
 		velocity.y=velocity.y+gravity*delta
 	
+func moverse(input_velocity):
+	velocity.x=(int(Input.is_action_pressed("ui_right"))-int(Input.is_action_pressed("ui_left")))*input_velocity
+	pass
 	
 
 	
