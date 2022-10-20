d7241531004382e85291e60b96531912f758fa7f
caasalazarsa <caasalazarsa@unal.edu.co>
2022-10-20 15:54:01 -0500

    all states commented

--- DEMO_PROJECT/scripts/player.gd
+++ DEMO_PROJECT/scripts/player.gd
@@ -1,18 +1,22 @@
 extends KinematicBody2D
 
-
+# variable de control máquina de estados
 var state =1
 
+# en el caso de los kinematic body se debe añadir el efecto gravitatorio
+# ya que el juego demo es un plataformero
 var gravity=400
 
-var walk_time_start=0
-var walk_time_now=0
-var walk_flag=false
+# velocidades de movimiento horizontal jump_velocity se asigna de acuerdo al
+# estado desde el cual se hace el salto
 export var walk_velocity=100
 export var run_velocity=400
 export var jump_velocity=0
+
+# velocidad de salto vertical
 export var jump_speed=600
 
+# vector en el cual se suman todas las velocidades
 var velocity=Vector2(0,0)
 # Declare member variables here. Examples:
 # var a = 2
@@ -31,8 +35,10 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	
+	# efecto gravitatorio continuo
 	gravity_effect(delta)
 	
+	# máquina de estados	
 	match state:
 		1:
 			quieto()
@@ -47,10 +53,19 @@ func _process(delta):
 	pass
 	
 func _physics_process(delta):
+	# con esta función el cuerpo puede moverse, colisionar y deslizarse sobre las plataformas
+	# el segundo parametro determina la normal de las superficies, es decir, la dirección en la
+	# perpendicular al suelo
 	move_and_slide(velocity,Vector2(0,-1))
 
+	## ESTADO QUIETO
+	# si se oprime <- o -> el jugador pasara de quieto a caminata
+	# si se oprime "ui_accept" el jugador para a salto
+	# las convenciones para las entradas están en 
+	# Proyecto>Configuración del proyecto>Mapa de entrada
 func quieto():
 	print("estoy quieto")
+	velocity.x=0
 	if(Input.is_action_pressed("ui_left") or Input.is_action_pressed("ui_right")):
 		state=2	
 		
@@ -63,8 +78,13 @@ func quieto():
 		
 	pass
 	
+	## ESTADO CAMINATA
+	# en este estado el jugador podrá moverse a la velocidad de caminata,
+	# hacer un salto si se oprime accept
+	# empezar a correr si se oprime cancel
+	
+	
 func caminata():
-	walk_time_now=OS.get_unix_time()
 	
 	moverse(walk_velocity)
 	
@@ -75,7 +95,7 @@ func caminata():
 		$Sprite.flip_h=true
 		print("camino hacia la izquierda")	
 	
-	if(Input.is_action_just_released("ui_left") or Input.is_action_just_released("ui_right")):
+	if(!Input.is_action_pressed("ui_left") and !Input.is_action_pressed("ui_right")):
 		state=1
 	if(Input.is_action_pressed("ui_cancel")):
 		state=3
@@ -87,6 +107,12 @@ func caminata():
 		
 	pass
 	
+	## ESTADO SPRINT
+	# en este estado el jugador podrá moverse a la velocidad de sprint,
+	# hacer un salto si se oprime accept
+	# empezar a caminar si se suelta el botón cancel
+	
+	
 func sprint():
 	
 	moverse(run_velocity)
@@ -96,9 +122,9 @@ func sprint():
 	if(Input.is_action_pressed("ui_left")):
 		$Sprite.flip_h=true
 		print("corro hacia la izquierda")
-	if(Input.is_action_just_released("ui_left") or Input.is_action_just_released("ui_right")):
+	if(!Input.is_action_pressed("ui_left") and !Input.is_action_pressed("ui_right")):
 		state=1
-	if Input.is_action_just_released("ui_cancel"):
+	if !Input.is_action_pressed("ui_cancel"):
 		state=2	
 		
 	if(Input.is_action_just_pressed("ui_accept") and is_on_floor()):
@@ -109,16 +135,28 @@ func sprint():
 	
 	pass	
 	
+	## ESTADO SALTO
+	# En este estado el jugador puede moverse a la velocidad de caminata o sprint
+	# acorde a la asignación de jump_velocity
+	# si toca el piso vuelve al estado quieto
+	# la función is on floor trabaja con la normal definida en move_and_slide()
+	
+	
 func salto():
 	moverse(jump_velocity)
 	print("estoy en el aire")
 	if(is_on_floor()):
 		state=1
 	pass
+	
+	# efecto gravitatorio en el personaje
 
 func gravity_effect(delta):
 	if velocity.y<400:
 		velocity.y=velocity.y+gravity*delta
+		
+	# función para moverse
+	# importante entender la lógica 
 	
 func moverse(input_velocity):
 	velocity.x=(int(Input.is_action_pressed("ui_right"))-int(Input.is_action_pressed("ui_left")))*input_velocity
